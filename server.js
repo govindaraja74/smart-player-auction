727be8c71454e0d8fba192f76dc27f458ea89678
govindaraja74 <govindaraja.bhat@gmail.com>
2026-07-31 08:30:06 +0530

    Increase JSON and URL-encoded body size limits

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
